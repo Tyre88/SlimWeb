5031f18cad3a4488e20ca1256db7d852bd3c61c5
Tyre88 <victor@webbdudes.se>
2017-02-26 15:56:20 +0100

    Starting to add the login module.

--- SW.Mediabank/DAL/MediabankDataSet.Designer.cs
+++ SW.Mediabank/DAL/MediabankDataSet.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\IIS-Sites\SlimWeb\Gradera.Mediabank\DAL\MediabankDataSet.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\IIS-Sites\SlimWeb\SW.Mediabank\DAL\MediabankDataSet.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- SlimWebClient/src/login/login.component.ts
+++ SlimWebClient/src/login/login.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'login',
     templateUrl: 'login.component.html',
-    styleUrls: ['login.component.css']
+    styleUrls: ['login.component.css'],
+    host: {'class': 'module-wrapper login-wrapper'}
 })
 export class LoginComponent {
 
